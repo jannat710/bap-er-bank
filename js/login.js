49c7b034b4b90b2d3f0ca97dfdf796cda2ac83cf
e <jannatul0040@gmail.com>
2023-08-15 23:27:55 +0600

    added all

--- js/login.js
+++ js/login.js
@@ -13,7 +13,7 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     
     // DANGER: DO NOT VERIFY email password on the client side 
     // step-4: verify email and password and check whether valid user or not
-    if(email === 'sontan@baap.com' && password === 'secret'){
+    if(email === 'jannat@fe.com' && password === 'secret'){
         window.location.href = 'bank.html';
     }
     else{
